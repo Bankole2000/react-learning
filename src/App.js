fc912df5545d8c0b56381a847f21ac5c56c62810
Esan Bankole <techybanky@gmail.com>
2019-05-15 17:30:19 +0100

    component state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
+      <Counter /> 
+      {/* <Greet name="Bruce" heroName="Batman">
         {" "}
         <p>Bruce Wayne? or Banner?</p>{" "}
       </Greet>
@@ -19,7 +21,7 @@ function App() {
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonderwoman" />
-      <h1>Something Else</h1>
+      <h1>Something Else</h1> */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    this.setState(
+      (prevState, props) => ({
+        count: prevState.count + 1
+      }),
+      () => console.log(this.state.count)
+    );
+    // console.log(this.state.count);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>count - {this.state.count}</div>
+        <button onClick={() => this.increment()}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
